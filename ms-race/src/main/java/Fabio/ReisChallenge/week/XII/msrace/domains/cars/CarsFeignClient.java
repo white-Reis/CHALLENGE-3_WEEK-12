594574d4bf51e11b2d130865bf8f2c526312f191
Fabio Reis <82837278+white-Reis@users.noreply.github.com>
2023-09-10 06:34:19 -0300

    compose_update

--- ms-race/src/main/java/Fabio/ReisChallenge/week/XII/msrace/domains/cars/CarsFeignClient.java
+++ ms-race/src/main/java/Fabio/ReisChallenge/week/XII/msrace/domains/cars/CarsFeignClient.java
@@ -3,7 +3,7 @@ package Fabio.ReisChallenge.week.XII.msrace.domains.cars;
 import Fabio.ReisChallenge.week.XII.msrace.domains.cars.enitys.Cars;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-@FeignClient(name = "cars-service", url = "http://0.0.0.0:8081/api/v1/cars")
+@FeignClient(name = "cars-service", url = "http://localhost:8081/api/v1/cars")
 public interface CarsFeignClient{
 
     @GetMapping
